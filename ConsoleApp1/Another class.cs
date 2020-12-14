af890808942353666e27b9792a0fae7c877b9fdf
pappu2020 <sahapappu018@gmail.com>
2020-12-14 10:52:02 +0600

    Second Commit

---
+++ ConsoleApp1/Another class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Another_class
+    {
+        public void Method()
+        {
+            Console.WriteLine("This is a another class");
+                
+        }
+    }
+}
